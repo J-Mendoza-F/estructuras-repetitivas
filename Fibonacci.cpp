5a790f8f0c5b51350d8d3a97e4d11ba92003a201
JMMendozaF <jair.mendoza.floresxd@gmail.com>
2024-06-15 09:35:48 -0500

    Se especficó la variable i diciendo que es un iterador y se arregló un error de la anterior versión, la variable n

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main(){
 	int num, a, b, c, i;
+	// i es un iterador para que el "mientras" (while) no se repita infinitamente
 	cout<<"SERIE FINOBACCI"<<endl;
 	cout<<"Introduzca el tope de la serie: ";
 	cin>>num;
 	i=1;
 	a=0;
 	b=1;
-	while (i<=n){
+	while (i<=num){
 		cout<<a<<endl;
 		c=a+b;
 		a=b;
